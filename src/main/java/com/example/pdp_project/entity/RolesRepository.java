93ebb0bad7156af3f7d907a85d4792d099e99d0e
sherali <nurmonoff@gmail.com>
2025-02-26 14:57:40 +0500

    mapper

--- src/main/java/com/example/pdp_project/entity/RolesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pdp_project.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RolesRepository extends JpaRepository<Roles, UUID> {
-}
