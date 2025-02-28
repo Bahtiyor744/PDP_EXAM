8cfbfd33061a6bf5535d9a54a199f80790b7d1ff
sherali <nurmonoff@gmail.com>
2025-03-01 00:45:53 +0500

    login

---
+++ src/main/java/com/example/pdp_project/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.example.pdp_project.service;
+
+import com.example.pdp_project.dto.UserDTO;
+import com.example.pdp_project.entity.User;
+import com.example.pdp_project.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+
+    public String login(String email, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        Optional<User> optionalUser = Optional.ofNullable(userRepository.findByEmail(email));
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("User not found");
+        }
+        return tokenService.generateToken(optionalUser.get());
+    }
+
+
+}
