6c74a87235147ce16001d0e750357d989ec5ce96
sherali <nurmonoff@gmail.com>
2025-03-01 06:42:55 +0500

    mapper

--- src/main/java/com/example/pdp_project/mapper/CourseMapper.java
+++ src/main/java/com/example/pdp_project/mapper/CourseMapper.java
@@ -5,7 +5,7 @@ import com.example.pdp_project.entity.Course;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CourseMapper {
     CourseMapper INSTANCE = Mappers.getMapper(CourseMapper.class);
 
