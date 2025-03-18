d538b9c82663d1d69af0ba7e0332fee859b12b07
sherali <nurmonoff@gmail.com>
2025-03-18 14:36:48 +0500

    Oauth2 bilan iwlaw uchun uzgartirildi

--- src/main/java/com/example/pdp_project/security/SecurityConfig.java
+++ src/main/java/com/example/pdp_project/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.pdp_project.security;
 
 import com.example.pdp_project.filters.MyFilter;
+import com.example.pdp_project.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,12 +18,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 public class SecurityConfig {
+
     private final CustomUserDetailsService customUserDetailsService;
     private final MyFilter myFilter;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService, MyFilter myFilter) {
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, MyFilter myFilter,
+                          CustomOAuth2UserService customOAuth2UserService,
+                          OAuth2SuccessHandler oAuth2SuccessHandler) {
         this.customUserDetailsService = customUserDetailsService;
         this.myFilter = myFilter;
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.oAuth2SuccessHandler = oAuth2SuccessHandler;
     }
 
     @Bean
@@ -32,21 +40,27 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
                         req
-                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                                .requestMatchers("/login","/login/code", "/file/**").permitAll()
-                                .requestMatchers("/user").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("ADMIN", "USER")
-                                .anyRequest()
-                                .authenticated())
+                                .requestMatchers(HttpMethod.OPTIONS, "/").permitAll()
+                                .requestMatchers("/login", "/file/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .successHandler(oAuth2SuccessHandler)  // SuccessHandler ni qo'shamiz
+                        .failureHandler((request, response, exception) -> {
+                            response.sendRedirect("/login?error=nouser");
+                        })
+                )
                 .userDetailsService(customUserDetailsService)
                 .addFilterBefore(myFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-     
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -60,6 +74,4 @@ public class SecurityConfig {
         authProvider.setUserDetailsService(customUserDetailsService);
         return authProvider;
     }
-
-
 }
